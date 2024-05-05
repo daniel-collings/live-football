a2cbedf78140d94d4064a8e6c767bf79a95148ab
Daniel Collings <collings8915@gmail.com>
2024-05-05 09:24:37 +0100

    Update Navbar

--- app/_components/layout/Navbar.tsx
+++ app/_components/layout/Navbar.tsx
@@ -12,20 +12,35 @@ import {
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon, RectangleGroupIcon } from '@heroicons/react/20/solid'
 import Link from "next/link";
 
-const products = [
+const macthes = [
     {
         name: 'Live',
-        description: 'Get a better understanding where your traffic is coming from',
-        href: '/matches/live',
+        description: 'Its game time! Find out the latest scores here',
+        href: '/live',
         icon: ChartPieIcon,
     },
     {
         name: 'Fixtures',
-        description: 'Speak directly to your customers with our engagement tool',
-        href: '/matches/fixtures',
+        description: 'Discover the big games that are coming up',
+        href: '/fixtures',
         icon: CursorArrowRaysIcon,
     },
-    { name: 'Results', description: 'Your customers’ data will be safe and secure', href: '/matches/results', icon: FingerPrintIcon },
+]
+
+const competitions = [
+    {
+        name: 'Continental',
+        description: 'Get a better understanding where your traffic is coming from',
+        href: '/competitions/continental',
+        icon: ChartPieIcon,
+    },
+    {
+        name: 'International',
+        description: 'Speak directly to your customers with our engagement tool',
+        href: '/competitions/internationals',
+        icon: CursorArrowRaysIcon,
+    },
+    { name: 'Domestic', description: 'Your customers’ data will be safe and secure', href: '/competitions/domestic', icon: FingerPrintIcon },
 ]
 // const callsToAction = [
 //     { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
@@ -42,12 +57,12 @@ export default function Navbar() {
 
     // @ts-ignore
     return (
-        <header className="relative bg-base-200 isolate z-10">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <header className="relative bg-base-100 isolate z-10">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-4" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <Link href="/" className="-m-1.5 p-1.5">
                         <span className="sr-only">{process.env.COMPANY_NAME}</span>
-                        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                        <img className="h-24 w-auto" src="/live-football.svg" alt="live-football" />
                     </Link>
                 </div>
                 <div className="flex lg:hidden">
@@ -61,76 +76,129 @@ export default function Navbar() {
                     </button>
                 </div>
                 <Popover.Group className="hidden lg:flex lg:gap-x-12">
-                    {/*<Popover>*/}
-                    {/*    <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6">*/}
-                    {/*        Matches*/}
-                    {/*        <ChevronDownIcon className="h-5 w-5 flex-none " aria-hidden="true"/>*/}
-                    {/*    </Popover.Button>*/}
+                    <Popover>
+                        <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6">
+                            Matches
+                            <ChevronDownIcon className="h-5 w-5 flex-none " aria-hidden="true"/>
+                        </Popover.Button>
 
-                    {/*    <Transition*/}
-                    {/*        as={Fragment}*/}
-                    {/*        enter="transition ease-out duration-200"*/}
-                    {/*        enterFrom="opacity-0 -translate-y-1"*/}
-                    {/*        enterTo="opacity-100 translate-y-0"*/}
-                    {/*        leave="transition ease-in duration-150"*/}
-                    {/*        leaveFrom="opacity-100 translate-y-0"*/}
-                    {/*        leaveTo="opacity-0 -translate-y-1"*/}
-                    {/*    >*/}
-                    {/*        <Popover.Panel*/}
-                    {/*            className="absolute inset-x-0 top-0 -z-10  pt-14 shadow-lg ring-1 ring-gray-900/5 bg-base-300">*/}
-                    {/*            <div*/}
-                    {/*                className="mx-auto grid max-w-7xl grid-cols-4 gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-8">*/}
-                    {/*                {products.map((item) => (*/}
-                    {/*                    <div key={item.name}*/}
-                    {/*                         className="group relative rounded-lg p-6 text-sm leading-6 hover:">*/}
-                    {/*                        <div*/}
-                    {/*                            className="flex h-11 w-11 items-center justify-center rounded-lg  group-hover:">*/}
-                    {/*                            <item.icon className="h-6 w-6  group-hover:text-primary"*/}
-                    {/*                                       aria-hidden="true"/>*/}
-                    {/*                        </div>*/}
-                    {/*                        <a href={item.href} className="mt-6 block font-semibold ">*/}
-                    {/*                            {item.name}*/}
-                    {/*                            <span className="absolute inset-0"/>*/}
-                    {/*                        </a>*/}
-                    {/*                        <p className="mt-1 ">{item.description}</p>*/}
-                    {/*                    </div>*/}
-                    {/*                ))}*/}
-                    {/*            </div>*/}
-                    {/*            /!*<div className="">*!/*/}
-                    {/*            /!*    <div className="mx-auto max-w-7xl px-6 lg:px-8">*!/*/}
-                    {/*            /!*        <div className="grid grid-cols-3 divide-x divide-gray-900/5 border-x border-gray-900/5">*!/*/}
-                    {/*            /!*            {callsToAction.map((item) => (*!/*/}
-                    {/*            /!*                <a*!/*/}
-                    {/*            /!*                    key={item.name}*!/*/}
-                    {/*            /!*                    href={item.href}*!/*/}
-                    {/*            /!*                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6  hover:bg-gray-100"*!/*/}
-                    {/*            /!*                >*!/*/}
-                    {/*            /!*                    <item.icon className="h-5 w-5 flex-none " aria-hidden="true" />*!/*/}
-                    {/*            /!*                    {item.name}*!/*/}
-                    {/*            /!*                </a>*!/*/}
-                    {/*            /!*            ))}*!/*/}
-                    {/*            /!*        </div>*!/*/}
-                    {/*            /!*    </div>*!/*/}
-                    {/*            /!*</div>*!/*/}
-                    {/*        </Popover.Panel>*/}
-                    {/*    </Transition>*/}
-                    {/*</Popover>*/}
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-200"
+                            enterFrom="opacity-0 -translate-y-1"
+                            enterTo="opacity-100 translate-y-0"
+                            leave="transition ease-in duration-150"
+                            leaveFrom="opacity-100 translate-y-0"
+                            leaveTo="opacity-0 -translate-y-1"
+                        >
+                            <Popover.Panel
+                                className="absolute inset-x-0 top-0 -z-10  pt-14 shadow-lg ring-1 ring-gray-900/5 bg-base-300">
+                                <div
+                                    className="mx-auto grid max-w-7xl grid-cols-4 gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-8">
+                                    {macthes.map((item) => (
+                                        <div key={item.name}
+                                             className="group relative rounded-lg p-6 text-sm leading-6 hover:">
+                                            <div
+                                                className="flex h-11 w-11 items-center justify-center rounded-lg  group-hover:">
+                                                <item.icon className="h-6 w-6  group-hover:text-primary"
+                                                           aria-hidden="true"/>
+                                            </div>
+                                            <a href={item.href} className="mt-6 block font-semibold ">
+                                                {item.name}
+                                                <span className="absolute inset-0"/>
+                                            </a>
+                                            <p className="mt-1 ">{item.description}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                                {/*<div className="">*/}
+                                {/*    <div className="mx-auto max-w-7xl px-6 lg:px-8">*/}
+                                {/*        <div className="grid grid-cols-3 divide-x divide-gray-900/5 border-x border-gray-900/5">*/}
+                                {/*            {callsToAction.map((item) => (*/}
+                                {/*                <a*/}
+                                {/*                    key={item.name}*/}
+                                {/*                    href={item.href}*/}
+                                {/*                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6  hover:bg-gray-100"*/}
+                                {/*                >*/}
+                                {/*                    <item.icon className="h-5 w-5 flex-none " aria-hidden="true" />*/}
+                                {/*                    {item.name}*/}
+                                {/*                </a>*/}
+                                {/*            ))}*/}
+                                {/*        </div>*/}
+                                {/*    </div>*/}
+                                {/*</div>*/}
+                            </Popover.Panel>
+                        </Transition>
+                    </Popover>
 
-                    <Link href="/matches" className="text-sm font-semibold leading-6 ">
-                        Matches
-                    </Link>
-                    <Link href="/competitions" className="text-sm font-semibold leading-6 ">
-                        Competitions
-                    </Link>
-                    <Link href="/competitions/leagues" className="text-sm font-semibold leading-6 ">
+                    {/*<Link href="/matches" className="text-sm font-semibold leading-6 ">*/}
+                    {/*    Matches*/}
+                    {/*</Link>*/}
+                    {/*<Link href="/competitions" className="text-sm font-semibold leading-6 ">*/}
+                    {/*    Competitions*/}
+                    {/*</Link>*/}
+                    <Popover>
+                        <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6">
+                            <Link href="/competitions">Competitions</Link>
+                            <ChevronDownIcon className="h-5 w-5 flex-none " aria-hidden="true"/>
+                        </Popover.Button>
+
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-200"
+                            enterFrom="opacity-0 -translate-y-1"
+                            enterTo="opacity-100 translate-y-0"
+                            leave="transition ease-in duration-150"
+                            leaveFrom="opacity-100 translate-y-0"
+                            leaveTo="opacity-0 -translate-y-1"
+                        >
+                            <Popover.Panel
+                                className="absolute inset-x-0 top-0 -z-10  pt-14 shadow-lg ring-1 ring-gray-900/5 bg-base-300">
+                                <div
+                                    className="mx-auto grid max-w-7xl grid-cols-4 gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-8">
+                                    {competitions.map((item) => (
+                                        <div key={item.name}
+                                             className="group relative rounded-lg p-6 text-sm leading-6 hover:">
+                                            <div
+                                                className="flex h-11 w-11 items-center justify-center rounded-lg  group-hover:">
+                                                <item.icon className="h-6 w-6  group-hover:text-primary"
+                                                           aria-hidden="true"/>
+                                            </div>
+                                            <a href={item.href} className="mt-6 block font-semibold ">
+                                                {item.name}
+                                                <span className="absolute inset-0"/>
+                                            </a>
+                                            <p className="mt-1 ">{item.description}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                                {/*<div className="">*/}
+                                {/*    <div className="mx-auto max-w-7xl px-6 lg:px-8">*/}
+                                {/*        <div className="grid grid-cols-3 divide-x divide-gray-900/5 border-x border-gray-900/5">*/}
+                                {/*            {callsToAction.map((item) => (*/}
+                                {/*                <a*/}
+                                {/*                    key={item.name}*/}
+                                {/*                    href={item.href}*/}
+                                {/*                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6  hover:bg-gray-100"*/}
+                                {/*                >*/}
+                                {/*                    <item.icon className="h-5 w-5 flex-none " aria-hidden="true" />*/}
+                                {/*                    {item.name}*/}
+                                {/*                </a>*/}
+                                {/*            ))}*/}
+                                {/*        </div>*/}
+                                {/*    </div>*/}
+                                {/*</div>*/}
+                            </Popover.Panel>
+                        </Transition>
+                    </Popover>
+                    <Link href="/leagues" className="text-sm font-semibold leading-6 ">
                         Leagues
                     </Link>
-                    <Link href="/news" className="text-sm font-semibold leading-6 ">
-                        News
+                    <Link href="/teams" className="text-sm font-semibold leading-6 ">
+                        Teams
                     </Link>
-                    <Link href="/company" className="text-sm font-semibold leading-6 ">
-                        Company
-                    </Link>
+
+
                 </Popover.Group>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                     <Link href="#" className="text-sm font-semibold leading-6 ">
@@ -140,15 +208,11 @@ export default function Navbar() {
             </nav>
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                 <div className="fixed inset-0 z-10" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-base-300  px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-base-300  px-3 py-3 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <Link href="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">{process.env.COMPANY_NAME}</span>
-                            <img
-                                className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                alt=""
-                            />
+                            <img className="h-24 w-auto" src="/live-football.svg" alt="live-football" />
                         </Link>
                         <button
                             type="button"
@@ -189,10 +253,16 @@ export default function Navbar() {
                                 {/*    )}*/}
                                 {/*</Disclosure>*/}
                                 <Link
-                                    href="/matches"
+                                    href="/live"
                                     className="-mx-3 block rounded-lg px-3 py-2  font-semibold leading-7  hover:"
                                 >
-                                    Matches
+                                    Live Matches
+                                </Link>
+                                <Link
+                                    href="/fixtures"
+                                    className="-mx-3 block rounded-lg px-3 py-2  font-semibold leading-7  hover:"
+                                >
+                                    Fixtures
                                 </Link>
                                 <Link
                                     href="/competitions"
@@ -201,23 +271,19 @@ export default function Navbar() {
                                     Competitions
                                 </Link>
                                 <Link
-                                    href="/competitions/leagues"
+                                    href="/leagues"
                                     className="-mx-3 block rounded-lg px-3 py-2  font-semibold leading-7  hover:"
                                 >
                                     Leagues
                                 </Link>
                                 <Link
-                                    href="/news"
+                                    href="/teams"
                                     className="-mx-3 block rounded-lg px-3 py-2  font-semibold leading-7  hover:"
                                 >
-                                    News
+                                    Teams
                                 </Link>
-                                <Link
-                                    href="/company"
-                                    className="-mx-3 block rounded-lg px-3 py-2  font-semibold leading-7  hover:"
-                                >
-                                    Company
-                                </Link>
+
+
                             </div>
                             <div className="py-6">
                                 <Link

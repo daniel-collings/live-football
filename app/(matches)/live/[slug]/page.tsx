32065adf491403c46f9bf533d5da05781d630a99
Daniel Collings <collings8915@gmail.com>
2024-05-14 20:43:33 +0100

    Add route for live match stats and update links within LiveScores.tsx component

---
+++ app/(matches)/live/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import PlaceholderDesign from "@/app/_components/PlaceholderDesign";
+
+export default function Page(){
+    return(
+        <PlaceholderDesign/>
+    )
+}
--- app/_components/LiveScores.tsx
+++ app/_components/LiveScores.tsx
@@ -3,6 +3,7 @@
 import React, {useState} from "react";
 import {ILiveScoreProps, IMatchData} from "@/app/(matches)/live/page";
 import Link from "next/link";
+import {stringToSlug} from "@/utils/urlSlugger";
 
 interface PageProps {
     params: { slug: string };
@@ -41,9 +42,7 @@ export default function Page({params, searchParams, matches}: PageProps) {
                 {filteredMatches.map(([leagueName, leagueMatches]) => (
                     <div key={leagueName}>
                         <div className="flex items-center mb-2">
-                            <Link href={`/international/teams/${leagueMatches[0].league.country}`}>
                                 <img src={leagueMatches[0].league.flag} alt="" className="w-6 h-4 mr-2" />
-                            </Link>
                             <Link href={`/leagues/${leagueName}-${leagueMatches[0].league.id}`}>
                                 <h2 className="text-md font-bold text-primary">{leagueName}</h2>
                             </Link>
@@ -53,20 +52,20 @@ export default function Page({params, searchParams, matches}: PageProps) {
                                 <div key={match.fixture.id} className="card bg-base-100 p-2 border-l-base-300 border-[1px] rounded-md">
                                     <div className="flex flex-row items-center justify-between">
                                         <div className="flex items-center space-x-2 w-1/3 md:min-w-44">
-                                            <Link href={`/teams/aston-villa-66`}>
+                                            <Link href={`/teams/${stringToSlug(match.teams.home.name)}-${match.teams.home.id}`}>
                                                 <img src={match.teams.home.logo} alt=""
                                                      className="w-6 h-6"/>
                                             </Link>
                                             <div className="truncate text-sm">{match.teams.home.name}</div>
                                         </div>
-                                        <Link href={`/matches/live/${match.fixture.id}`} className="w-1/3">
+                                        <Link href={`/live/${match.fixture.id}`} className="w-1/3">
                                             <div className="grow text-center items-center  mx-4">
                                                 {match.goals.home} - {match.goals.away}<small className="animate-pulse opacity-5">{' '}<sup>({match.fixture.status.elapsed})</sup></small>
                                             </div>
                                         </Link>
                                         <div className="flex items-center space-x-2 w-1/3 md:min-w-44 justify-end">
                                             <div className="truncate text-sm">{match.teams.away.name}</div>
-                                            <Link href={`/`}>
+                                            <Link href={`/teams/${stringToSlug(match.teams.away.name)}-${match.teams.away.id}`}>
                                                 <img src={match.teams.away.logo} alt="" className="w-6 h-6" />
                                             </Link>
                                         </div>
@@ -79,7 +78,7 @@ export default function Page({params, searchParams, matches}: PageProps) {
             </div>
 
             <div className="mt-4 text-center">
-                <Link href="/matches/live" className="text-primary font-bold">
+                <Link href="/live" className="text-primary font-bold">
                     View All Live Matches
                 </Link>
             </div>

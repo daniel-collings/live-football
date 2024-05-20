2e97180f6cdb4235a739b754f3c1b03acd712fdf
Daniel Collings <collings8915@gmail.com>
2024-05-20 21:21:12 +0100

    Add images to leagues

--- app/leagues/page.tsx
+++ app/leagues/page.tsx
@@ -4,6 +4,7 @@ import ConstraintLayoutTemplate from "@/app/_components/template/ConstraintLayou
 import Image from "next/image";
 import {stringToSlug} from "@/utils/urlSlugger";
 import Link from "next/link";
+import React, {useState} from "react";
 
 const data: CountryLeagues = {
     "England": [
@@ -20,18 +21,30 @@ const data: CountryLeagues = {
         {
             leagueName: "League One",
             leagueId: 41,
-            leagueImage: "/league-logos/ligue-1-logo.png",
+            leagueImage: "/league-logos/league1.jpg",
         },
         {
             leagueName: "League Two",
             leagueId: 42,
-            leagueImage: "/league-logos/ligue-1-logo.png",
+            leagueImage: "/league-logos/efl2.jpg",
         },
         {
             leagueName: "National League",
             leagueId: 43,
-            leagueImage: "/league-logos/ligue-1-logo.png",
+            leagueImage: "/league-logos/national-league.jpg",
         },
+    ],
+    "Spain": [
+        {
+            leagueName: "La Liga",
+            leagueId: 1,
+            leagueImage: "/league-logos/la-liga-logo.jpg",
+        },
+        {
+            leagueName: "La Liga 2",
+            leagueId: 1,
+            leagueImage: "/league-logos/la-liga-2.jpg",
+        }
     ],
     "Germany": [
         {
@@ -42,7 +55,7 @@ const data: CountryLeagues = {
         {
             leagueName: "2. Bundesliga",
             leagueId: 1,
-            leagueImage: "/league-logos/ligue-1-logo.png",
+            leagueImage: "/league-logos/bundesliga-2-logo.png",
         }
     ],
     "France": [
@@ -54,19 +67,7 @@ const data: CountryLeagues = {
         {
             leagueName: "Ligue 2",
             leagueId: 1,
-            leagueImage: "/league-logos/mls-logo.jpg",
-        }
-    ],
-    "Spain": [
-        {
-            leagueName: "La Liga",
-            leagueId: 1,
-            leagueImage: "/league-logos/la-liga-logo.jpg",
-        },
-        {
-            leagueName: "La Liga 2",
-            leagueId: 1,
-            leagueImage: "/league-logos/mls-logo.jpg",
+            leagueImage: "/league-logos/ligue2.jpg",
         }
     ],
     "Italy": [
@@ -78,29 +79,29 @@ const data: CountryLeagues = {
         {
             leagueName: "Serie B",
             leagueId: 1,
-            leagueImage: "/league-logos/mls-logo.jpg",
+            leagueImage: "/league-logos/serie-b.jpeg",
         }
     ],
     "Scotland": [
         {
             leagueName: "Scottish Premier League",
             leagueId: 1,
-            leagueImage: "/league-logos/mls-logo.jpg",
+            leagueImage: "/league-logos/spl.jpg",
         },
         {
             leagueName: "Scottish Championship",
             leagueId: 1,
-            leagueImage: "/league-logos/mls-logo.jpg",
+            leagueImage: "/league-logos/s-champ.png",
         },
         {
             leagueName: "Scottish League 1",
             leagueId: 1,
-            leagueImage: "/league-logos/mls-logo.jpg",
+            leagueImage: "/league-logos/sl1.jpg",
         },
         {
             leagueName: "Scottish League 2",
             leagueId: 1,
-            leagueImage: "/league-logos/mls-logo.jpg",
+            leagueImage: "/league-logos/sl2.png",
         }
     ],
     "United States": [
@@ -123,27 +124,54 @@ interface CountryLeagues {
 }
 
 export default function Page(){
-        return(
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredData = Object.entries(data).reduce((acc, [country, leagues]) => {
+        const filteredLeagues = leagues.filter((league) =>
+            league.leagueName.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        if (filteredLeagues.length > 0) {
+            acc[country] = filteredLeagues;
+        }
+        return acc;
+    }, {} as CountryLeagues);
+
+
+    return(
         <div className="">
             <ConstraintLayoutTemplate>
                 <div className="space-y-8">
-                    {Object.keys(data).map((country: string, i:number) => (
+                    <input
+                        type="text"
+                        placeholder="Search by league or country"
+                        value={searchTerm}
+                        onChange={handleSearch}
+                        className="input input-bordered w-full my-4"
+                    />
+
+                    {Object.keys(filteredData).map((country: string, i: number) => (
                         <div key={i}>
                             <p className="font-bold text-lg">{country}</p>
 
                             <div className="carousel carousel-end md:gap-x-4 p-4 bg-base-300 space-x-4 rounded-box">
-                                {data[country].map((league: League, index: number) => (
+                                {filteredData[country].map((league: League, index: number) => (
 
                                     <div className="carousel-item" key={index}>
                                         <Link href={`/leagues/${stringToSlug(league.leagueName)}`}>
-                                        <Image src={league.leagueImage}
-                                               width={200}
-                                               height={200}
-                                               className="rounded-box max-h-56 h-full w-auto" alt=""/>
-                                        </Link>
+                                            <div className="flex flex-col h-full space-y-4">
+                                                <Image src={league.leagueImage}
+                                                       width={200}
+                                                       height={200}
+                                                       className="bg-white rounded-box max-h-56 h-full w-auto" alt={league.leagueName}/>
+<p>{league.leagueName}</p>
+                                            </div>
+                                                 </Link>
                                     </div>
                                 ))}
-
                             </div>
                         </div>
 
@@ -151,14 +179,8 @@ export default function Page(){
 
                 </div>
 
-                {/*<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-between gap-8">*/}
-                {/*    {_leagues.response.map((o: ILeagueProps, i: number) => (*/}
-                {/*        <LeagueCard key={i} league={o.league} country={o.country}/>*/}
-                {/*    ))}*/}
-                {/*</div>*/}
             </ConstraintLayoutTemplate>
 
         </div>
         )
 }
-

2feecd2fe13f5d9ab94cb7c554753c1ca906435d
Daniel Collings <collings8915@gmail.com>
2024-05-30 14:55:21 +0100

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/app/_components/layout/Navbar";
 import Footer from "@/app/_components/layout/Footer";
 import {FC, ReactNode} from "react";
 import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,15 +27,16 @@ const RootLayout: FC<RootLayoutProps> = ({ children}) => {
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE_ADSENSE}`}
             crossOrigin="anonymous"
         />
-      <body className={`text-base-content {inter.className}`}>
+        <GoogleAnalytics gaId="G-ET2SFF42X4" />
+        <body className={`text-base-content ${inter.className}`}>
         <Navbar/>
         <main>
             {/*<QueryClientProvider client={queryClient}>*/}
             {children}
             {/*</QueryClientProvider>*/}
         </main>
-      <Footer/>
-      </body>
+        <Footer/>
+        </body>
 
     </html>
   );
